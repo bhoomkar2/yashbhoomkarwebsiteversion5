16b3298d14b2813504f0bc03a5be2c82df4795ca
bhoomkar2 <159558627+bhoomkar2@users.noreply.github.com>
2024-07-23 00:45:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
+          <Route path="/" element={<Home/>} />
+
           <Route path="/home" element={<Home/>} />
           
           <Route path="/about" element={<About/>} />
